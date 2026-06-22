51f0b0db3748a15f3cba2bd9bde90e756ff7f142
astogeee510 <pabloastroza671@gmail.com>
2026-06-22 05:14:56 -0400

    eliminacion de carpeta ms_modificado con microservicios obsoletos

--- ms_modificado/GESTIONACADEMICO/src/main/java/com/pablo/GESTIONACADEMICO/repository/AsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pablo.GESTIONACADEMICO.repository;
-
-import com.pablo.GESTIONACADEMICO.model.Asignatura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
-
-    boolean existsByNombreIgnoreCase(String nombre);
-}
